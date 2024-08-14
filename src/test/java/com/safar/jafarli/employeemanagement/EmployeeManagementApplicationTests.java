aed701472b18cf29738634850d996606e2a27559
Sefer22 <sfrs87997@gmail.com>
2024-08-14 18:07:40 +0400

    em

---
+++ src/test/java/com/safar/jafarli/employeemanagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safar.jafarli.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
