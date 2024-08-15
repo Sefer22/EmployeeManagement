924191b10f63b4ffc78ecc43f8cb912d950e1d9e
Sefer22 <sfrs87997@gmail.com>
2024-08-15 20:03:53 +0400

    em

--- src/main/java/com/safar/jafarli/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/safar/jafarli/employeemanagement/controller/EmployeeController.java
@@ -1,8 +1,13 @@
 package com.safar.jafarli.employeemanagement.controller;
 
+import com.safar.jafarli.employeemanagement.entity.Employee;
 import com.safar.jafarli.employeemanagement.service.impl.EmployeeServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -12,5 +17,8 @@ public class EmployeeController {
 
     public EmployeeController(EmployeeServiceImpl employeeService) {
         this.employeeService = employeeService;
+    }
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        return new ResponseEntity<>(employeeService.getAllEmployees(), HttpStatus.OK);
     }
 }
--- src/main/java/com/safar/jafarli/employeemanagement/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/safar/jafarli/employeemanagement/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,21 @@
 package com.safar.jafarli.employeemanagement.service.impl;
 
+import com.safar.jafarli.employeemanagement.entity.Employee;
+import com.safar.jafarli.employeemanagement.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeServiceImpl {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
 }
