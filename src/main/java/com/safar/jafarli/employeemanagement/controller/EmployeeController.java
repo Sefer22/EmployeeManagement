4e19b511d35656d25b98df9c9dcfabcfc39d141f
Sefer22 <sfrs87997@gmail.com>
2024-08-15 21:16:45 +0400

    em

--- src/main/java/com/safar/jafarli/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/safar/jafarli/employeemanagement/controller/EmployeeController.java
@@ -4,8 +4,7 @@ import com.safar.jafarli.employeemanagement.entity.Employee;
 import com.safar.jafarli.employeemanagement.service.impl.EmployeeServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,7 +17,24 @@ public class EmployeeController {
     public EmployeeController(EmployeeServiceImpl employeeService) {
         this.employeeService = employeeService;
     }
+    @GetMapping("/all")
     public ResponseEntity<List<Employee>> getAllEmployees() {
         return new ResponseEntity<>(employeeService.getAllEmployees(), HttpStatus.OK);
+    }
+    @PostMapping("/new")
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
+        return new ResponseEntity<>(employeeService.createEmployee(employee),HttpStatus.CREATED);
+    }
+    @GetMapping("/{employeeId}")
+    public ResponseEntity<Employee> findEmployeeById(@PathVariable Long employeeId) {
+        return new ResponseEntity<>(employeeService.findEmployeeById(employeeId),HttpStatus.OK);
+    }
+    @PutMapping("/edit/{employeeId}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long employeeId,@RequestBody Employee newEmployee) {
+        return new ResponseEntity<>(employeeService.updateEmployee(employeeId,newEmployee),HttpStatus.OK);
+    }
+    @DeleteMapping("/delete/{employeeId}")
+    public void removeEmployee(@PathVariable Long employeeId) {
+        employeeService.removeEmployee(employeeId);
     }
 }
--- src/main/java/com/safar/jafarli/employeemanagement/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/safar/jafarli/employeemanagement/service/impl/EmployeeServiceImpl.java
@@ -2,9 +2,12 @@ package com.safar.jafarli.employeemanagement.service.impl;
 
 import com.safar.jafarli.employeemanagement.entity.Employee;
 import com.safar.jafarli.employeemanagement.repository.EmployeeRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl {
@@ -17,5 +20,35 @@ public class EmployeeServiceImpl {
 
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee findEmployeeById(Long employeeId) {
+        Optional<Employee> employee = employeeRepository.findById(employeeId);
+        if(employee.isPresent()) {
+            return employee.get();
+        }
+        return null;
+    }
+
+    public Employee updateEmployee(Long employeeId, Employee newEmployee) {
+        Optional<Employee> foundedEmployee = employeeRepository.findById(employeeId);
+        if(foundedEmployee.isPresent()) {
+            Employee updateEmployee = foundedEmployee.get();
+            updateEmployee.setId(newEmployee.getId());
+            updateEmployee.setFirstName(newEmployee.getFirstName());
+            updateEmployee.setLastName(newEmployee.getLastName());
+            updateEmployee.setEmail(newEmployee.getEmail());
+
+            return employeeRepository.save(updateEmployee);
+        }
+        return null;
+    }
+
+    public void removeEmployee(Long employeeId) {
+        employeeRepository.deleteById(employeeId);
     }
 }
